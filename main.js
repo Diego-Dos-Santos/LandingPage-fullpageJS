ffa88415ae361579beb998928c74d8ffa5207424
Diego-Dos-Santos <diegodossantos.es@gmail.com>
2020-04-11 23:16:08 +0200

    fullPageJS

---
+++ main.js
@@ -0,0 +1,9 @@
+new fullpage('#fullPage', {
+    autoScrolling: true,
+    navigation: true,
+    anchors: ['section1','section2','section3','section4'],
+    navigationTooltips:['Home','About','Contacto','Redes Sociales'],
+    showActiveTooltip: true,
+    scrollingSpeed: 1300,
+    
+})
